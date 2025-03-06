cbb67723fab09b4676098d675ee8d1f3dfae7809
u7861723 <u7861723@anu.edu.au>
2025-03-06 14:47:54 +1100

    finished

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
 var app = express();
@@ -11,7 +12,17 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const upload = multer({ storage: multer.memoryStorage() });
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const file = req.file;
 
+  // 响应上传文件的元数据：文件名，类型，大小
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
